4b028ce7e8c932656617be3f5f5fd135925b361d
Thomas Ballinger <thomasballinger@gmail.com>
2016-04-01 13:53:47 -0400

    variable rename

--- server.py
+++ server.py
@@ -10,13 +10,13 @@ def web_server():
         mysocket.listen(5)
 
         while True:
-            sockt, stuff = mysocket.accept()
+            sockt, address = mysocket.accept()
             try:
                 sockt.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
                 got_msg = sockt.recv(1000)
                 if got_msg.find('GET /favicon.ico') != -1:
                     continue
-                print stuff, got_msg
+                print address, got_msg
                 num = np.random.rand()
                 msg_send = "My random number: {0}\r\n".format(num)
                 print 'Sending {0}'.format(msg_send)
